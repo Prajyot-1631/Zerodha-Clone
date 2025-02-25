1d95ca57b74fa889811afe6f6936c67a06970551
Prajyot Shejwal <prajyotshejwal@gmail.com>
2025-02-25 20:06:44 +0530

    Pricing Component

--- frontend/src/landing_page/pricing/PricingPage.jsx
+++ frontend/src/landing_page/pricing/PricingPage.jsx
@@ -1,10 +1,12 @@
 import Brokerage from "./Brokerage";
 import Hero from "./Hero";
+import OpenAccount from "../OpenAccount";
 
 function PricingPage() {
   return (
     <>
       <Hero />
+      <OpenAccount />
       <Brokerage />
     </>
   );
