cab01f35ecc2fe15a3ac093fac6ab7f02e3915d2
Prajyot Shejwal <prajyotshejwal@gmail.com>
2025-02-27 02:12:35 +0530

    Established connection with MongoDB Server

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+const URI = process.env.MONGO_URL;
 
 const app = express();
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log("server started");
+  mongoose.connect(URI);
+  console.log("DB Connected");
 });
