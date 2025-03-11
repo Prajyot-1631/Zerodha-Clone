fd91b9c36fc8a291fed769c2b70d190c7d987692
Prajyot Shejwal <prajyotshejwal@gmail.com>
2025-03-12 02:09:34 +0530

    Solved the issue of external token sharing.

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,8 +1,22 @@
 // import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./components/Home";
 
 function App() {
+  useEffect(() => {
+    //window.location.search = gets the part of the URL after " ? "
+    //new URLSearchParams(...) = helps extract the token from it.
+    const urlParams = new URLSearchParams(window.location.search);
+    const tokenFromURL = urlParams.get("token");
+
+    if (tokenFromURL) {
+      //if token found , save it in url
+      localStorage.setItem("token", tokenFromURL);
+      //window.history.replaceState(...) = updates the url to remove text after " ? "
+      window.history.replaceState({}, document.title, "/");
+    }
+  }, []);
   return (
     <>
       <Routes>
